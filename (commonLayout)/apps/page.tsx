9b43badf776849796397d0d76235c2998c700f65
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2025-04-06 01:00:34 +0200

    Add files via upload

---
+++ (commonLayout)/apps/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useContextSelector } from 'use-context-selector'
+import { useTranslation } from 'react-i18next'
+import Apps from './Apps'
+import AppContext from '@/context/app-context'
+import { LicenseStatus } from '@/types/feature'
+
+const AppList = () => {
+  const { t } = useTranslation()
+  const systemFeatures = useContextSelector(AppContext, v => v.systemFeatures)
+
+  return (
+    <div className='relative flex flex-col overflow-y-auto bg-background-body shrink-0 h-0 grow'>
+      <Apps />
+      {systemFeatures.license.status === LicenseStatus.NONE && <footer className='px-12 py-6 grow-0 shrink-0'>  
+      </footer>}
+    </div >
+  )
+}
+
+export default AppList
---
+++ (commonLayout)/datasets/DatasetFooter.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useTranslation } from 'react-i18next'
+
+const DatasetFooter = () => {
+  const { t } = useTranslation()
+}
+
+export default DatasetFooter
